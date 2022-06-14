cd11bca030a41673dd6a78b6772f1ceccb0d9d4a
Stephanie Conde <stephaniecondecareer@gmail.com>
2022-06-13 18:52:07 -0700

    commit tree afb0fa0de9a440a3690942edde757f438a6818ed

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+function saturdayFun(activity){
+    if (!activity) {
+        return 'This Saturday, I want to roller-skate!'
+    } else {
+        return (`This Saturday, I want to ${activity}!`)}
+}
+
+function mondayWork(activity1) {
+    if(!activity1){
+        return 'This Monday, I will go to the office.'
+    } else {
+        return (`This Monday, I will ${activity1}.`)
+    }
+}
+
+function wrapAdjective (part1 = "*") {
+    return function(adjective = 'special') {
+        return `You are ${part1}${adjective}${part1}!`
+    }
+}
+
